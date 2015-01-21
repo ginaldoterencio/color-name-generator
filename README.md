ec11205548b117e11cb44f25ef7410c606ac49f7
Ginaldo Terencio <ginaldoterencio@gmail.com>
2015-01-21 15:47:10 -0200

    ADD: validacao de parametros, Instrucoes no readme

--- README.md
+++ README.md
@@ -2,3 +2,41 @@ color-name-generator
 ====================
 
 Generate color name according with the base color name and group
+
+# Color name generator
+
+
+## Getting Started
+
+- Install: `npm install -g color-name-generator`
+- Run generator: `yo nh-sass`
+- Run grunt: `grunt watch`
+
+
+## Options
+
+* `-h, --help`
+
+  Output usage information
+
+* `-V, --version`
+
+  Output the version number
+
+* `-c, --color`
+
+  Hexadecimal color
+
+* `-b, --baseColor`
+
+  Hexadecimal base color
+
+* `-g, --groupColorName`
+
+  Group color name
+
+
+## License
+
+[BSD license](http://opensource.org/licenses/bsd-license.php)
+
--- generate-color-name.js
+++ generate-color-name.js
@@ -5,12 +5,34 @@ var tinycolor = require('tinycolor2'),
 	program = require('commander');
 
 program
-    .version('0.0.1')
-    // .usage('<keywords>')
-    .option('-c, --color [name]', 'Color')
-    .option('-b, --baseColor [name]', 'Base color')
-    .option('-g, --groupColorName [name]', 'Group color name color')
+    .version('0.1.2')
+    .usage('[options] <file ...>')
+    .option('-c, --color [hexadecimal]', 'Color')
+    .option('-b, --baseColor [hexadecimal]', 'Base color')
+    .option('-g, --groupColorName [string]', 'Group color name')
     .parse(process.argv);
+
+var hexPattern = /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i,
+    isParamsValid = true;
+
+if((typeof program.color !== 'string') || !(hexPattern.test(program.color))) {
+  console.log(chalk.red('Please, type a valid color.'));
+  isParamsValid = false;
+}
+
+if((typeof program.baseColor !== 'string') || !(hexPattern.test(program.baseColor))) {
+  console.log(chalk.red('Please, type a valid base color.'));
+  isParamsValid = false;
+}
+
+if((typeof program.groupColorName !== 'string') || (program.groupColorName.trim() === '')) {
+  console.log(chalk.red('Type a group color name'));
+  isParamsValid = false;
+}
+
+if(!isParamsValid) {
+  return;
+}
 
 var baseColor = tinycolor(program.baseColor).toHsl();
 var color = tinycolor(program.color).toHsl();
@@ -25,10 +47,10 @@ var diff = {
 console.log(chalk.green('$%s-group-%s-%s-color: ' + 
 			chalk.blue('hsl-diff($%s-color, %s, %s, %s);'+ 
 			chalk.gray(' //%s'))),
-			program.group,
+			program.groupColorName,
 			lig,
 			parseInt(intense, 10),
-			program.group,
+			program.groupColorName,
 			diff.h,
 			diff.s,
 			diff.l,
